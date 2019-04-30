589a84477b16c2ec0038da33ddd685dab91e1005
Preston deSouza <desouzapreston@gmail.com>
2019-04-30 11:36:42 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { About } from './about/about.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    component: About
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+
