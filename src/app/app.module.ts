6fdb4b87d4317e44cc222d8e602c2448547006b2
Preston deSouza <desouzapreston@gmail.com>
2019-05-01 10:38:45 -0500

    Home Component Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AboutComponent } from './about/about.component';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppRoutingModule } from './app-routing.module';
 import { DonateComponent } from './donate/donate.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    DonateComponent
+    DonateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
