ca50e40d846908ea1e3e42bd863c2ddadca45bde
Preston deSouza <desouzapreston@gmail.com>
2019-05-28 19:34:40 -0500

    Firebase update

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,16 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// const firebase = require("firebase");
+// // Required for side-effects
+// require("firebase/firestore");
+
+// // Initialize Cloud Firestore through Firebase
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyBlFFQvwW_34alMcvoeL3Mb02sAVWTii6E',
+//   authDomain: 'paperstraws-51033.firebaseapp.com',
+//   projectId: 'paperstraws-51033'
+// });
+
+// var db = firebase.firestore();
