8d5223c6adb08894f8713f947904bcbffe868527
Preston deSouza <desouzapreston@gmail.com>
2019-06-19 14:54:55 -0500

    Contact Form Additions

--- src/app/home/contact-form.component.ts
+++ src/app/home/contact-form.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+// import { ErrorStateMatcher } from '@angular/material/core';
+// import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
-// This is to check our input
-class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched ));
-  }
-}
-// || control.isSubmitted
+// // This is to check our input
+// class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched ));
+//   }
+// }
+// // || control.isSubmitted
+
 
 @Component({
   selector: 'app-contact-form',
@@ -19,17 +20,21 @@ class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 
 export class AppContactForm implements OnInit {
-  matcher = new MyErrorStateMatcher();
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+  // matcher = new MyErrorStateMatcher();
+  email: string;
+  name: string;
+  message: string;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    let parm = this.route.snapshot.params['parm']
-    console.log("ngOnInit", parm)
+    // let parm = this.route.snapshot.params['parm']
+    console.log("ngOnInit")
+  }
+
+  submitMessage() {
+    const messageForm = `My name is ${this.name}.\nMy email is ${this.email}.\nHere is my message: ${this.message}`;
+    alert(messageForm);
   }
 }
 
